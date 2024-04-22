ce014498602c45543c15e1d54a07753864b46554
Yappy0 <83028586+Yappy0@users.noreply.github.com>
2024-04-22 11:20:03 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ var slidesRequest = new XMLHttpRequest();
 slidesRequest.open("GET", "https://csclub.pythonanywhere.com/static/slides.json", false);
 slidesRequest.send();
 if (slidesRequest.status == 200){
-  document.getElementById("").innerHTML = slidesRequest.responseText;
+  document.getElementById("slides-section").innerHTML = slidesRequest.responseText;
 }
