2be08d7ad7a0484195a972482af877d6c68926fb
Yappy0 <83028586+Yappy0@users.noreply.github.com>
2024-04-23 08:31:57 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ slidesRequest.send();
 if (slidesRequest.status == 200){
   slides = JSON.parse(slidesRequest.responseText);
   content = "";
-  for (const [key, value of Object.entries(slides)]){
+  for (const [key, value] of Object.entries(slides)]){
     content += "<div class=\"content\"><h1>" + key + "</h1><a href=\"" + value + "\">Slides</a></div>";
   }
   document.getElementById("slides-section").innerHTML = content;
